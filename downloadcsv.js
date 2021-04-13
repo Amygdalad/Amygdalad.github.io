6cef2d4b0a3b9b68c576bc98af1f41685994a241
Amygdalad <amygdalad@gmail.com>
2021-04-13 17:12:00 -0500

    Test

---
+++ downloadcsv.js
@@ -0,0 +1,8 @@
+function download_csv(csv) {
+	console.log(csv);
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'tempdata.csv';
+    hiddenElement.click();
+}
---
+++ websockets.js
@@ -0,0 +1,48 @@
+﻿// This is the JavaScript file for the basic LabVIEW WebSockets demo.
+// Copyright © 2016 by MediaMongrels Ltd. 
+
+	function connect(){  
+		var socket;
+		const alldata = "";
+			
+		
+		// The LabVIEW demo uses port 6123, but this can be changed to any port. Change 'localhost' if connecting to a server running on another IP address.
+		var host = "ws://10.0.4.108:6124";  
+
+		try{  
+			var socket = new WebSocket(host);  
+  
+			message('<p class="event">Connecting...');  
+			
+			// Tell the user the connection has been established
+			socket.onopen = function(){  
+				message('<p class="event">Connected');
+				message('<p class="event">Downloading Data...');				
+			}  
+			
+			// Display the received message
+			socket.onmessage = function(msg){ 		
+				message('<p class="message">'+msg.data);
+				//message('<p class="message">'+alldata.concat(msg.data);
+			}  
+			
+			// Tell the user the connection has been closed
+			socket.onclose = function(){
+				message('<p class="event">Socket Status: '+socket.readyState+' (Closed)'); 
+				message('<p class="event">' +typeof alldata);
+				message('<p class="event">'+alldata);
+				
+						
+			
+			}           
+  
+		} catch(exception){  
+			message('<p>Error'+exception);  
+		}    
+		
+		// Add the message to the log (and close the paragraph)
+		function message(msg){  
+			$('#chatLog').append(msg+'</p>');  
+		}  
+   	return alldata;
+	} //End connect
